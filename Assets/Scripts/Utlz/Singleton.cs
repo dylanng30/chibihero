4bc374f961155de8b6968e8f933c1152c05f1ab7
DylanNguyen3010 <dylan.ng3010@gmail.com>
2025-05-17 20:41:24 +0700

    Update Singleton.cs

--- Assets/Scripts/Utlz/Singleton.cs
+++ Assets/Scripts/Utlz/Singleton.cs
@@ -18,7 +18,10 @@ public class Singleton<T> : StaticInstance<T> where T : MonoBehaviour
     protected override void Awake()
     {
         if (Instance != null)
+        {        
             Destroy(gameObject);
+            return;
+        }
         base.Awake();
     }
 }
