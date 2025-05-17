c8bffececebe7d9455598677f2dc20bdb73ce198
DylanNguyen3010 <dylan.ng3010@gmail.com>
2025-05-17 20:54:26 +0700

    Delete Assets/Scripts/Scriptable/SceneEnemyConfig.cs

--- Assets/Scripts/Scriptable/SceneEnemyConfig.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "SceneEnemyConfig", menuName = "Enemy System/Scene Enemy Config")]
-public class SceneEnemyConfig : ScriptableObject
-{
-    public string sceneName;
-    public List<EnemyData> enemies;
-}
